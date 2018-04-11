fc82e282d69316665bd8fa470506487b5b55faa4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-10 19:20:06 -0700

    robot color chooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -10,33 +10,45 @@ import org.jointheleague.graphical.robot.Robot;
 import java.awt.Color;
 
 public class RobotColorChooser {
+
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		Robot r2d2= new Robot();
-		//3. Ask the user what color they would like the robot to draw
-		String color=JOptionPane.showInputDialog(null, "what color would you like?");
-		//5. Use an if/else statement to set the pen color that the user requested
-if(color.equals("red")) {
-	r2d2.setPenColor(color.red);
-}
-else if(color.equals("black"))
-	r2d2.setPenColor(.black);
 
-//6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		// 1. Create a new Robot
 		
-		//4. Set the pen width to 10
+		Robot r2d2 = new Robot();
+		r2d2.penDown();
+		for (int i = 0; i < 5; i++) {
+		// 3. Ask the user what color they would like the robot to draw
+		String color = JOptionPane.showInputDialog(null, "what color would you like?");
+		// 5. Use an if/else statement to set the pen color that the user requested
+		if (color.equals("red")) {
+			r2d2.setPenColor(Color.RED);
+		} else if (color.equals("black")) {
+			r2d2.setPenColor(Color.black);
+		} else if (color.equals("blue")) {
+			r2d2.setPenColor(Color.blue);
+		}
+
+		// 6. If the user doesn’t enter anything, choose a random color
+		else {
+			r2d2.setRandomPenColor();
+		}
+		
+		// 7. Put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+			
+		
+		// 4. Set the pen width to 10
 		r2d2.setPenWidth(10);
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-r2d2.move (100);
-r2d2.turn(90);
-r2d2.move(100);
-r2d2.turn(90);
-r2d2.move(100);
-r2d2.turn(90);
+		
+		// 2. Make the robot draw a shape (this will take more than one line of code)
+		for (int j = 0; j < 4; j++) {
+		
+		r2d2.move(100);
+		r2d2.turn(90);
 
-
-
+		}
+		}
 	}
 }
