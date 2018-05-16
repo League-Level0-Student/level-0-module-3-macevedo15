a9b7ae472f39639b8aa8aa30f7a45b48b4c693c7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-15 19:21:16 -0700

    fizzbizz

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -27,5 +27,17 @@ package modulo;
 
 public class FizzBuzz {
 
+	public static void main(String[] args) {
+	
+		for (int i = 1; i <=20;i++) {
+		System.out.println(i);		
+	if(i%3==0) {
+		syso("fizz");
+		
+	}
+		
+		
+	}
+}
 }
 
